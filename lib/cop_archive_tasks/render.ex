ec6bdc4b6eafadb8cf05b43f0f3904d9b0248f2d
Jonathan Yankovich <jonathan.yankovich@gmail.com>
2019-11-15 09:26:35 -0600

    Add the layout to the rendered output.

--- lib/cop_archive_tasks/render.ex
+++ lib/cop_archive_tasks/render.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Render do
   use Mix.Task
 
   alias CopArchive.{Forum, Topic, Reply, User}
-  alias CopArchiveWeb.{ForumView, TopicView, ReplyView, UserView, PageView}
+  alias CopArchiveWeb.{ForumView, TopicView, ReplyView, UserView, PageView, LayoutView}
 
   use CopArchiveWeb, :view
 
@@ -19,18 +19,35 @@ defmodule Mix.Tasks.Render do
   end
 
   defp render_menu() do
-    str = Phoenix.View.render_to_string(PageView, "index.html", conn: conn())
+    str =
+      Phoenix.View.render_to_string(PageView, "index.html",
+        conn: conn(),
+        layout: {LayoutView, "app.html"}
+      )
+
     write("/index", str)
   end
 
   defp render_forums() do
     forums = Forum.all(order: :date, preload: [:topics])
 
-    str = Phoenix.View.render_to_string(ForumView, "index.html", conn: conn(), forums: forums)
+    str =
+      Phoenix.View.render_to_string(ForumView, "index.html",
+        conn: conn(),
+        forums: forums,
+        layout: {LayoutView, "app.html"}
+      )
+
     write("/forum/index", str)
 
     Enum.each(forums, fn forum ->
-      str = Phoenix.View.render_to_string(ForumView, "show.html", conn: conn(), forum: forum)
+      str =
+        Phoenix.View.render_to_string(ForumView, "show.html",
+          conn: conn(),
+          forum: forum,
+          layout: {LayoutView, "app.html"}
+        )
+
       Routes.forum_path(conn(), :show, forum) |> write(str)
     end)
   end
@@ -38,11 +55,23 @@ defmodule Mix.Tasks.Render do
   defp render_topics() do
     topics = Topic.all(order: :date, preload: [:forum, :user, replies: :user])
 
-    str = Phoenix.View.render_to_string(TopicView, "index.html", conn: conn(), topics: topics)
+    str =
+      Phoenix.View.render_to_string(TopicView, "index.html",
+        conn: conn(),
+        topics: topics,
+        layout: {LayoutView, "app.html"}
+      )
+
     write("/topic/index", str)
 
     Enum.each(topics, fn topic ->
-      str = Phoenix.View.render_to_string(TopicView, "show.html", conn: conn(), topic: topic)
+      str =
+        Phoenix.View.render_to_string(TopicView, "show.html",
+          conn: conn(),
+          topic: topic,
+          layout: {LayoutView, "app.html"}
+        )
+
       Routes.topic_path(conn(), :show, topic) |> write(str)
     end)
   end
@@ -50,12 +79,24 @@ defmodule Mix.Tasks.Render do
   defp render_replies() do
     replies = Reply.all(order: :date, preload: [:topic, :user])
 
-    str = Phoenix.View.render_to_string(ReplyView, "index.html", conn: conn(), replies: replies)
+    str =
+      Phoenix.View.render_to_string(ReplyView, "index.html",
+        conn: conn(),
+        replies: replies,
+        layout: {LayoutView, "app.html"}
+      )
+
     write("/reply/index", str)
 
     Enum.each(replies, fn reply ->
       if reply.topic do
-        str = Phoenix.View.render_to_string(ReplyView, "show.html", conn: conn(), reply: reply)
+        str =
+          Phoenix.View.render_to_string(ReplyView, "show.html",
+            conn: conn(),
+            reply: reply,
+            layout: {LayoutView, "app.html"}
+          )
+
         Routes.reply_path(conn(), :show, reply) |> write(str)
       end
     end)
@@ -64,11 +105,23 @@ defmodule Mix.Tasks.Render do
   defp render_users() do
     users = User.all(order: :name, preload: [:topics, :replies])
 
-    str = Phoenix.View.render_to_string(UserView, "index.html", conn: conn(), users: users)
+    str =
+      Phoenix.View.render_to_string(UserView, "index.html",
+        conn: conn(),
+        users: users,
+        layout: {LayoutView, "app.html"}
+      )
+
     write("/user/index", str)
 
     Enum.each(users, fn user ->
-      str = Phoenix.View.render_to_string(UserView, "show.html", conn: conn(), user: user)
+      str =
+        Phoenix.View.render_to_string(UserView, "show.html",
+          conn: conn(),
+          user: user,
+          layout: {LayoutView, "app.html"}
+        )
+
       Routes.user_path(conn(), :show, user) |> write(str)
     end)
   end
