df47c0c10c0ea8bf7fb83f3ca41d01b54c46e405
Jonathan Yankovich <jonathan.yankovich@gmail.com>
2019-11-15 14:07:28 -0600

    Format topic names.

--- lib/cop_archive_web/router.ex
+++ lib/cop_archive_web/router.ex
@@ -13,6 +13,8 @@ defmodule CopArchiveWeb.Router do
     plug :accepts, ["json"]
   end
 
+  get "/", CopArchiveWeb.ForumController, :index
+
   scope "/cop-archive", CopArchiveWeb do
     pipe_through :browser
 
